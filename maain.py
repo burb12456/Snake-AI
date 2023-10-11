bcd8081f0123c1f7a7eda355389eec1746169d0a
Burgh <tim.the.roadrunner8@gmail.com>
2023-10-11 17:13:01 +0100

    init

--- maain.py
+++ maain.py
@@ -1,18 +1,20 @@
 import pygame
 import sys
 import random
+import heapq
 
 # Initialize Pygame
 pygame.init()
 
 # Constants
-WIDTH, HEIGHT = 500, 500
-GRID_SIZE = 30
+WIDTH, HEIGHT = 750, 750
+GRID_SIZE = 40
 GRID_WIDTH = WIDTH // GRID_SIZE
 GRID_HEIGHT = HEIGHT // GRID_SIZE
 WHITE = (0, 255, 0)
 RED = (255, 0, 0)
 GREEN = (0, 0, 255)
+DARK_GREEN = (0, 50, 255)
 SNAKE_SPEED = 2  # Adjust this as needed
 CHEEDAR = 5
 SCORE = 0
@@ -39,14 +41,18 @@ class Snake:
             self.body.pop()
 
     def change_direction(self, new_direction):
-        if new_direction == "UP" and self.direction != (0, 1):
-            self.direction = (0, -1)
-        elif new_direction == "DOWN" and self.direction != (0, -1):
-            self.direction = (0, 1)
-        elif new_direction == "LEFT" and self.direction != (1, 0):
-            self.direction = (-1, 0)
-        elif new_direction == "RIGHT" and self.direction != (-1, 0):
-            self.direction = (1, 0)
+        if (
+            (new_direction == "UP" and self.direction != (0, 1)) or
+            (new_direction == "DOWN" and self.direction != (0, -1)) or
+            (new_direction == "LEFT" and self.direction != (1, 0)) or
+            (new_direction == "RIGHT" and self.direction != (-1, 0))
+        ):
+            self.direction = {
+                "UP": (0, -1),
+                "DOWN": (0, 1),
+                "LEFT": (-1, 0),
+                "RIGHT": (1, 0)
+            }[new_direction]
 
     def check_collision(self):
         if (
@@ -73,18 +79,65 @@ class Food:
 
     def draw(self):
         pygame.draw.rect(screen, RED, (self.position[0] * GRID_SIZE, self.position[1] * GRID_SIZE, GRID_SIZE, GRID_SIZE))
-
+def heuristic(a, b):
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
 # A* Pathfinding
 def find_path(snake, food):
-    # If A* fails, just move towards the food
-    if snake.body[0][0] < food.position[0]:
-        return "RIGHT"
-    elif snake.body[0][0] > food.position[0]:
-        return "LEFT"
-    elif snake.body[0][1] < food.position[1]:
-        return "DOWN"
-    elif snake.body[0][1] > food.position[1]:
-        return "UP"
+    # Create a graph representing the game grid
+    graph = [[0 for _ in range(GRID_WIDTH)] for _ in range(GRID_HEIGHT)]
+
+    # Mark the snake's body as obstacles
+    for segment in snake.body:
+        graph[segment[1]][segment[0]] = -1
+
+    open_set = []
+    closed_set = set()
+    came_from = {}
+
+    start = snake.body[0]
+    goal = food.position
+
+    heapq.heappush(open_set, (0, start))
+
+    while open_set:
+        current_cost, current_node = heapq.heappop(open_set)
+
+        if current_node == goal:
+            path = []
+            while current_node in came_from:
+                path.append(current_node)
+                current_node = came_from[current_node]
+            path.reverse()  # Reverse the path to follow from head to tail
+
+            if snake.grow:
+                snake.grow = False  # Reset the grow flag
+                return  # The snake will continue moving in the same direction
+
+            # Set the snake's direction based on the next step in the path
+            next_step = path[0]
+            snake.direction = tuple(map(lambda x, y: x - y, next_step, snake.body[0]))
+            return
+
+        closed_set.add(current_node)
+
+        for neighbor in [(current_node[0] + 1, current_node[1]),
+                        (current_node[0] - 1, current_node[1]),
+                        (current_node[0], current_node[1] + 1),
+                        (current_node[0], current_node[1] - 1)]:
+            if (
+                0 <= neighbor[0] < GRID_WIDTH
+                and 0 <= neighbor[1] < GRID_HEIGHT
+                and graph[neighbor[1]][neighbor[0]] != -1
+                and neighbor not in closed_set
+            ):
+                tentative_cost = current_cost + 1
+                if (
+                    (tentative_cost, neighbor) not in open_set
+                    or tentative_cost < graph[neighbor[1]][neighbor[0]]
+                ):
+                    came_from[neighbor] = current_node
+                    graph[neighbor[1]][neighbor[0]] = tentative_cost
+                    heapq.heappush(open_set, (tentative_cost + heuristic(neighbor, goal), neighbor))
 
 # Initialize Snake and Food
 snake = Snake()
@@ -101,13 +154,12 @@ while True:
 
     # Call your AI logic to determine the next direction for the snake
     if frame_count % SNAKE_SPEED == 0:
-        direction = find_path(snake, food)
-        snake.change_direction(direction)
+        find_path(snake, food)
         snake.move()
 
     # Check for collision with food
     if snake.body[0] == food.position:
-        snake.grow = False
+        snake.grow = True
         SCORE += 1
         food.respawn()
 
@@ -127,7 +179,7 @@ while True:
     pygame.display.flip()
 
     # Control the game speed
-    clock.tick(60)  # 60 frames per second
+    clock.tick(10)  # 60 frames per second
     frame_count += 1
     print("Game loop running")
-    print(SCORE)
+    print("Score:", SCORE)
